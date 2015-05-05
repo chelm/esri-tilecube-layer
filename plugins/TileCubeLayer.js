017f108cdee46e9986bdabc1b5025fbf46ce1409
brendan <brendan.heberton@gmail.com>
2015-05-05 11:10:26 -0600

    add temporal example

--- plugins/TileCubeLayer.js
+++ plugins/TileCubeLayer.js
@@ -38,6 +38,8 @@ var PbfTileLayer = declare(VectorTileLayer, {
   constructor: function ( urlTemplate, options) {
     this.inherited(arguments);
     this.tileQ = queue(4);
+    this.temporal = options.temporal || true;
+    this.timeIndex = options.startTime || 10; 
   },
 
   // override this method to create canvas elements instead of img
@@ -64,10 +66,7 @@ var PbfTileLayer = declare(VectorTileLayer, {
               }, 25);
             }, id);
            // for saving data, store the tile and layers
-           // self._tileData[id] = {
-           //   layers: layers,
-           //   tile: tile
-           // }
+           self._tileData[id] = tileJson;
             //self._render(element, tile, id, function(){
             //});
           } catch(e){
@@ -120,35 +119,87 @@ var PbfTileLayer = declare(VectorTileLayer, {
     }
   },
 
+  _update: function( styles ){
+    this.styles = styles;
+    for (var id in this._tileData){
+      this._render( this._tileDom[id], this._tileData[id], id, id, function(){} );
+    }
+  },
+
   _render: function(canvas, tile, id, tileid, callback){
     var start = Date.now();
     var self = this;
     var context = canvas.getContext('2d');
     var width = canvas.width, height=canvas.height;
-    console.log(width, height);
-    //context.clearRect(0, 0, width, height);
+    //console.log(width, height);
 
-    for (var time in tile){
-      if (this.hidpi) {
-        width *= (1/window.devicePixelRatio);
-        height *= (1/window.devicePixelRatio);
-      }
+    context.clearRect(0, 0, width, height);
 
-      // parse the renderer into a fill
-      for (var i = 0; i < tile[time].length; i++){
-        //console.log(tile[time][i])
-        this._renderPoint( tile[time][i], context, tileid ); 
-      }
+    if (this.hidpi) {
+      width *= (1/window.devicePixelRatio);
+      height *= (1/window.devicePixelRatio);
     }
-    this._tilePerf[id] = (Date.now() - start);
+
+    if ( this.temporal === true ) {
+      var time = self.timeIndex;
+
+      for (var t = 0; t < time; t++){
+        if ( tile[t] ) {
+          for (var i = 0; i < tile[t].length; i++){
+            this._renderPoint( tile[t][i], context, tileid );
+          }
+        }
+      }
+
+    } else {
+      
+      for (var time in tile){
+        // parse the renderer into a fill
+        for (var i = 0; i < tile[time].length; i++){
+          //console.log(tile[time][i])
+          this._renderPoint( tile[time][i], context, tileid ); 
+        }
+      }
+
+    }
+
+    //this._tilePerf[id] = (Date.now() - start);
     callback();
 
   },
 
   _renderPoint: function(point, context, tileid){
-    context.lineWidth = 1;
-    context.fillStyle = 'rgb(100,100,100)';
-    context.strokeStyle = 'rgb(200,200,200)';
+    
+    var stylePoint = function(val) {
+      var color;
+      switch(true) {
+        case val === 0:
+            color ='rgb(255,255,178)';
+            break;
+        case val === 1:
+            color ='rgb(254,217,118)';
+            break;
+        case val === 2:
+            color ='rgb(254,178,76)';
+            break;
+        case val === 3:
+            color ='rgb(253,141,60)';
+            break;
+        case val === 4:
+            color ='rgb(240,59,32)';
+            break;
+        case val === 5:
+            color ='rgb(189,0,38)';
+            break;
+        default:
+            color = 'rgb(250,250,250)';
+      }
+      return color;
+    }
+
+    context.lineWidth = 0.8;
+    context.fillStyle = stylePoint(parseInt(point.v));
+    context.strokeStyle = 'rgb(240,240,240)';
 
     var x = point.x;
     var y = point.y;
